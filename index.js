f6e41440536f9985491fe3e241a58b6296ff2790
TheBekker <christianbekker@gmail.com>
2021-01-08 20:43:35 +0100

    base typescript etc setup

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8181;
-app.get('/', (req,res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
