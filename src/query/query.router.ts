ab91c78b1f4a2895282037f1b2135ba6a20c9cc8
TheBekker <christianbekker@gmail.com>
2022-03-23 19:21:31 +0100

    improve template and cache logic + allow empty template for raw json response

--- src/query/query.router.ts
+++ src/query/query.router.ts
@@ -5,21 +5,49 @@ import { off } from 'process';
 export const queryRouter = express.Router();
 const fs = require('fs');
 const NodeCache = require( 'node-cache' );
-const myCache = new NodeCache({ stdTTL: 30, checkperiod: 120 });
+const queryCache = new NodeCache({ stdTTL: 30, checkperiod: 120 });
+
+const parseTemplate = (request: Request, response: gamedig.QueryResult) => {
+    const type: Type = request.params.type as Type;
+    const host: string = request.params.host;
+    const port: number = parseInt(request.params.port);
+    const template: string = decodeURI(request.query.template as string);
+
+    const players_amount = response.players.length||'-',
+        players_max = response.maxplayers,
+        server_ping = response.ping,
+        server_connect = response.connect;
+
+    const parsed = template
+        .replace(/{type}/g, type.toString())
+        .replace(/{host}/g, host)
+        .replace(/{port}/g, port.toString())
+        .replace(/{players_amount}/g, players_amount.toString())
+        .replace(/{players_max}/g, players_max.toString())
+        .replace(/{server_ping}/g, server_ping.toString())
+        .replace(/{server_connect}/g, server_connect);
+    return parsed; 
+};
 
 queryRouter.get('/:type/:host/:port', async (req: Request, res: Response) => {
     try {
         const type: Type = req.params.type as Type;
         const host: string = req.params.host;
         const port: number = parseInt(req.params.port);
-        const outputTemplate: string = decodeURI(req.query.template as string);
+        const cacheKey: string = type + host + port;
+        
         let offlineMessage: string = decodeURI(req.query.offline_message as string);
         if(!req.query.offline_message) {
             offlineMessage = 'Server offline';
         }
 
-        if ( myCache.has( type + host + port )) {
-            res.status(200).json(myCache.get( type + host + port ));
+        if (queryCache.has(cacheKey)) {
+            const gamedigReponse = queryCache.get(cacheKey);
+            if(req.query.template) {
+                res.status(200).send(parseTemplate(req, gamedigReponse));
+            } else {
+                res.status(200).json(gamedigReponse);
+            }
         } else {
             gamedig.query({
                 type: type,
@@ -27,25 +55,12 @@ queryRouter.get('/:type/:host/:port', async (req: Request, res: Response) => {
                 port: port,
                 givenPortOnly: true
             }).then(r => {
-                const players_amount = r.players.length||'-',
-                    players_max = r.maxplayers,
-                    server_ping = r.ping,
-                    server_connect = r.connect;
-
-    	    if (req.query.template) {
-    	        const renderLine = outputTemplate
-                        .replace(/{type}/g, type.toString())
-                        .replace(/{host}/g, host)
-                        .replace(/{port}/g, port.toString())
-                        .replace(/{players_amount}/g, players_amount.toString())
-                        .replace(/{players_max}/g, players_max.toString())
-                        .replace(/{server_ping}/g, server_ping.toString())
-                        .replace(/{server_connect}/g, server_connect);
-    	        res.status(200).send(renderLine);
-    	    } else {
-                myCache.set( type + host + port , r);
-    	        res.status(200).json(r);
-    	    }
+                queryCache.set(cacheKey , r);
+                if(req.query.template) {
+                    res.status(200).send(parseTemplate(req, r));
+                } else {
+                    res.status(200).json(r);
+                }
             }).catch(e => {
                 res.status(500).send(offlineMessage);
             });
@@ -54,6 +69,8 @@ queryRouter.get('/:type/:host/:port', async (req: Request, res: Response) => {
         res.status(404).send(e.message);
     }
 });
+
+
 
 queryRouter.get('/types', async (req: Request, res: Response) => {
     fs.readFile('./node_modules/gamedig/games.txt', 'utf8' , (err : any, data : any) => {
